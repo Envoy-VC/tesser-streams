6c4754e8545b257e29e35d570c01acf85d380686
Vedant Chainani <vedantchainani1084@gmail.com>
2025-06-02 04:11:35 +0530

    chore: add e2e test for vesting schedule

--- packages/contracts/test/TesserVesting.t.sol
+++ packages/contracts/test/TesserVesting.t.sol
@@ -7,6 +7,7 @@ import {SetUp} from "test/base/SetUp.sol";
 
 // Libraries
 import {TesserProxyLib} from "src/libraries/TesserProxyLib.sol";
+import {VestingStorageLib} from "src/libraries/VestingStorage.sol";
 
 // Interfaces
 import {IDiamondLoupe} from "src/interfaces/IDiamondLoupe.sol";
@@ -38,8 +39,8 @@ contract TesserVestingTests is Test, SetUp {
     function test_createVestingSchedule() public {
         vm.startBroadcast(alice.addr);
         uint256 totalAmount = 1000e18;
-        uint40 cliffDuration = 3 days;
-        uint40 vestingDuration = 30 days;
+        uint40 cliffDuration = 30 days;
+        uint40 vestingDuration = 365 days;
         uint256 alpha = 0.5e18;
 
         tesserToken.mint(alice.addr, totalAmount);
@@ -49,19 +50,76 @@ contract TesserVestingTests is Test, SetUp {
             bob.addr, address(tesserToken), totalAmount, cliffDuration, vestingDuration, alpha
         );
 
-        uint256 length = (vestingDuration + cliffDuration) / 1 days;
+        uint256 vestedAmount = getVestedAmount(vestingId);
+        uint256 releasableAmount = getReleasableAmount(vestingId);
+        assertEq(vestedAmount, 0);
+        assertEq(releasableAmount, 0);
 
-        TimeSlots[] memory timeSlots = new TimeSlots[](length);
+        // After Cliff duration vested, releasable amount should be equal 0
+        vm.warp(block.timestamp + cliffDuration);
+        vestedAmount = getVestedAmount(vestingId);
+        releasableAmount = getReleasableAmount(vestingId);
 
-        for (uint256 i = 0; i < length; i++) {
-            vm.warp(block.timestamp + 1 days);
-            timeSlots[i].timestamp = block.timestamp + i * 1 days;
-            timeSlots[i].vestedAmount = getVestedAmount(vestingId);
-            timeSlots[i].releasableAmount = getReleasableAmount(vestingId);
-        }
+        // After Half vesting duration, vested amount should be equal to (0.5)^alpha * totalAmount
+        // that is (0.5)^0.5 * 950 = 671
+        /// 950 because of 5% protocol fee
+        VestingStorageLib.VestingSchedule memory schedule = vestingCoreFacet.getVestingSchedule(vestingId);
+        vm.warp(block.timestamp + vestingDuration / 2 + schedule.startTime);
+        vestedAmount = getVestedAmount(vestingId);
+        releasableAmount = getReleasableAmount(vestingId);
 
-        for (uint256 i = 0; i < length; i++) {
-            console.log(i, timeSlots[i].releasableAmount);
-        }
+        assertEq(vestedAmount, 671);
+        assertEq(releasableAmount, 671);
+
+        vm.stopBroadcast();
+
+        // Now we release tokens
+        vm.startBroadcast(bob.addr);
+        uint256 bobBalance = tesserToken.balanceOf(bob.addr);
+        vestingCoreFacet.release(vestingId);
+        bobBalance = tesserToken.balanceOf(bob.addr) / 1e18;
+        assertEq(bobBalance, 671);
+        vm.stopBroadcast();
+
+        // After releasing tokens vested amount should be equal 671 but releasable amount should be 0
+        vestedAmount = getVestedAmount(vestingId);
+        releasableAmount = getReleasableAmount(vestingId);
+
+        assertEq(vestedAmount, 671);
+        assertEq(releasableAmount, 0);
+
+        // After more 1/4 of vesting duration, vested amount should be equal to (0.75)^0.5 * totalAmount
+        // that is (0.75)^0.5 * 950 = 822
+        // Vested amount should be 822 but releasable amount should be 822 - 671 = 150 (approximation)
+
+        vm.warp(block.timestamp + vestingDuration / 4);
+        vestedAmount = getVestedAmount(vestingId);
+        releasableAmount = getReleasableAmount(vestingId);
+
+        assertEq(vestedAmount, 822);
+        assertEq(releasableAmount, 150);
+
+        // after more 1/4 of vesting duration, releasable amount should be 950 - 671 = 278(approximation)
+        vm.warp(block.timestamp + vestingDuration / 4);
+        vestedAmount = getVestedAmount(vestingId);
+        releasableAmount = getReleasableAmount(vestingId);
+
+        assertEq(vestedAmount, 950);
+        assertEq(releasableAmount, 278);
+
+        // Claim all releasable amount
+        vm.startBroadcast(bob.addr);
+        vestingCoreFacet.release(vestingId);
+        bobBalance = tesserToken.balanceOf(bob.addr);
+        vm.stopBroadcast();
+
+        assertEq(bobBalance, 950e18);
+
+        // After releasing all tokens, vested amount should be equal to total amount
+        vestedAmount = getVestedAmount(vestingId);
+        releasableAmount = getReleasableAmount(vestingId);
+
+        assertEq(vestedAmount, 950);
+        assertEq(releasableAmount, 0);
     }
 }
